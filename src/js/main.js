33d245a9d26b29f59c8cbeb4c2691ee83fee60ca
Anna <apelsann@ya.ru>
2017-01-31 16:48:00 +0300

    slider

--- src/js/main.js
+++ src/js/main.js
@@ -32,3 +32,42 @@ $('.info__open').on('click', function(){
 	$(this).parents('.info__item').find('.info__btn').toggleClass('info__btn_open');
 	$(this).parents('.info__item').find('.info__content').slideToggle();
 });
+
+//slider
+
+var slider = document.getElementById('range_mileage');
+
+noUiSlider.create(slider, {
+	start: 0,
+	connect: [true, false],
+	step: 15000,
+	range: {
+		'min': 15000,
+		// '30 000': 20,
+		// '45 000': 30,
+		// '60 000': 40,
+		// '75 000': 50,
+		// '90 000': 60,
+		// '105 000': 70,
+		// '120 000': 80,
+		// '135 000': 90,
+		'max': 150000
+	}
+
+});
+
+
+
+slider.noUiSlider.on('update', function( values, handle ) {
+	var value_slider =  slider.noUiSlider.get();
+	// alert(typeof(value_slider));
+
+	// var v = 'v' + value_slider;
+	// var value_s = String(value_slider);
+	// alert(value_s);
+	// if ($('#mileage').val() == value_slider) {
+	// 	$(this).attr("selected", "selected");
+	// }
+	$("#mileage option[value=" + String(slider.noUiSlider.get()) + "]").attr("selected", "selected");
+
+});
